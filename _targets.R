29d45e9d25bb88c00b09ce703b1cb8f4b20acecc
juan-gallo <juanfgallo@experimentality.co>
2026-03-25 18:58:08 -0500

    feat: influentia plot

--- _targets.R
+++ _targets.R
@@ -4,21 +4,33 @@ source("./install_dependencies.R")
 source("./data_definition.R")
 source("./1_box_plot.R")
 source("./2_ANOVA.R")
+source("./3_regression_validation.R")
 
+# Dependencies installation
 install_dependencies()
 
+# Data definitions
 resistors <- get_resistors()
 techniques <- get_techniques()
 aggregated_df <- get_aggregated()
 resistors_df <- get_data_frame_resistors(resistors, techniques)
 
-
+# Boxplot
 boxplot <- get_box_plot(resistors_df, resistors, techniques)
 print(boxplot)
 
+# Regression and ANOVA 
 regression_model = get_model(resistors, techniques, resistors_df)
-
 print_anova(regression_model)
-
 show_summary(regression_model)
+
+# ----------------------------------------------------
+# 3. Validación de supuestos del modelo (normalidad, 
+# homocedasticidad, independencia, influencia)
+# ----------------------------------------------------
+
+validation_of_assumptions(regression_model,  resistors, techniques, resistors_df)
+
+
+
 
